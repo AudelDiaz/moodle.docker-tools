a62b9e98c756aced538e396d402dcb17adbcd5bd
Audel Diaz <adn7@hotmail.com>
2016-05-09 23:39:48 -0500

    pruebas saber

--- images/apache/scripts/start-services.sh
+++ images/apache/scripts/start-services.sh
@@ -2,25 +2,17 @@
 
 set -e
 
-# Download Moodle and deploy it to the MOODLE_WWW_ROOT
-if [[ ! "$(ls ${MOODLE_WWW_ROOT})" ]]; then
-    echo "Copying Moodle ..."
-    cp -r -v /opt/moodle ${WWW_ROOT}
-    chown -R www-data:www-data ${WWW_ROOT}
-    mv ${MOODLE_WWW_ROOT}/.git ${MOODLE_WWW_ROOT}/.git-history-backup
-    cat > ${WWW_ROOT}/index.html <<- EOM
+chown -R www-data:www-data ${WWW_ROOT}
+mv ${MOODLE_WWW_ROOT}/.git ${MOODLE_WWW_ROOT}/.git-history-backup
+cat > ${WWW_ROOT}/html/index.html <<- EOM
 <html>
     <head>
-        <meta http-equiv="refresh" content="0; url=/moodle">
+        <meta http-equiv="refresh" content="0; url=/pruebas_saber">
     </head>
 <body></body>
 </html>
 EOM
 
-
-else
-    echo "Moodle folder exists..."
-fi
 
 ## Start Apache2
 service apache2 start
